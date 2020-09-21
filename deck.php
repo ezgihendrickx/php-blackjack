166048e49fdc7c3490f6a4cdf864b7f4f89098cd
ezgihendrickx <ezgihendrickx@gmail.com>
2020-09-21 10:32:03 +0200

    Add OOP files

---
+++ deck.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+class Deck
+{
+    private const CARDS_PER_SUIT = 14;
+
+    private $cards = [];
+
+    public function __construct()
+    {
+        $suits = [
+            Suit::SPADE(),
+            Suit::HEART(),
+            Suit::CLUB(),
+            Suit::DIAMOND(),
+        ];
+
+        foreach ($suits as $suit) {
+            foreach (range(1, self::CARDS_PER_SUIT) as $i) {
+                $this->cards[] = new Card($suit, $i);
+            }
+        }
+    }
+
+    public function shuffle(): void
+    {
+        shuffle($this->cards);
+    }
+
+    /** @return Card[] */
+    public function getCards(): array
+    {
+        return $this->cards;
+    }
+
+    public function drawCard(): ?Card
+    {
+        return array_shift($this->cards);
+    }
+}
